9b2c52564be93cb17cb61304c53e8b633ebf6373
Luftalian <springtechtree@gmail.com>
2024-06-02 21:27:10 +0900

    Bot8

--- env.fish
+++
@@ -1,4 +0,0 @@
-set -x ACCESS_TOKEN "tBcWmVUiHCUHfqItpjWQ7I0GpqyqEmBvWovG"
-set -x BOT_MENTION "!{\"type\":\"user\",\"raw\":\"@BOT_TodaysTalks\",\"id\":\"145dc90e-f8f7-48b9-b313-a2073fd3e5eb\"}"
-set -x BOT_ID "36f45ddd-ee7d-4fd0-bcf2-92b3bf222c2c"
-set -x BOT_NAME "@BOT_TodaysTalks"
