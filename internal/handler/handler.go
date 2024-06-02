c989067c341d01c19474c131153012f485c9cd12
Luftalian <springtechtree@gmail.com>
2024-06-02 20:16:08 +0900

    Bot2

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -25,5 +25,5 @@ func (h *Handler) SetupSubscriptionEvent(bot *traqwsbot.Bot) {
 }
 
 func (h *Handler) SetUpCron(c *cron.Cron) {
-	c.AddFunc("15 * * * * *", h.OnCronHandler)
+	c.AddFunc("20 * * * * *", h.OnCronHandler)
 }
